0ccc8e0e646a52f5922358a9a8c8e2c9b34fbb4a
zyqentechnologies <zyqentechnologies@gmail.com>
2026-09-25 00:11:07 +0500

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,8 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttpModule from "pino-http";
 import router from "./routes";
-import { logger } from "./lib/logger";
 
 const app: Express = express();
-
-const pinoHttp =
-  (pinoHttpModule as typeof import("pino-http")).default ??
-  (pinoHttpModule as unknown as typeof import("pino-http"));
-
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req: (req: any) => ({
-        id: req.id,
-        method: req.method,
-        url: req.url?.split("?")[0],
-      }),
-      res: (res: any) => ({
-        statusCode: res.statusCode,
-      }),
-    },
-  }),
-);
 
 app.use(cors());
 app.use(express.json());
